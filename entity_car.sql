e770746b723494136c1c6802255c6e2fd85bd8df
Irucha <kurnosova.iryna@gmail.com>
2017-04-21 11:40:19 +0300

    changed car

--- entity_car.sql
+++ entity_car.sql
@@ -1,7 +1,7 @@
 create table STUDENT_CAR(
     STUDENT_EMAIL_FK varchar2(20)
-  , STUDENT_CAR_NO  varchar2(20),
-  student_car_model varchar2(20)
+  , STUDENT_CAR_NO  varchar2(20)
+  ,student_car_model varchar2(20)
 )
 
 alter table STUDENT_CAR add constraint sudent_car_PK primary key (STUDENT_EMAIL_FK, STUDENT_CAR_NO);
