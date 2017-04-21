a3037a0c939fe06b477808250c956880daded9f4
Paul <b.paul4@gmail.com>
2017-04-21 10:15:14 +0300

    Added Comment for student.

--- entity_student.sql
+++ entity_student.sql
@@ -1,6 +1,7 @@
 create table STUDENT(
- STUDENT_EMAIL varchar2(20) not null
-,STUDENT_NAME varchar2(20) not null
+  STUDENT_EMAIL varchar2(20) not null
+, STUDENT_NAME varchar2(20) not null
+, STUDENT_COMMENT varchar2(20)
 );
 
 alter table STUDENT add constraint sudent_pk PRIMARY KEY(STUDENT_EMAIL);
