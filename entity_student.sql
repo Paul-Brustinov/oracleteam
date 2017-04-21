f0a27ffe06fe92ec25e7626c6690f87991c3c9c5
Paul <b.paul4@gmail.com>
2017-04-21 11:23:03 +0300

    merge with team

--- entity_student.sql
+++
@@ -1,7 +0,0 @@
-create table STUDENT(
-  STUDENT_EMAIL varchar2(20) not null
-, STUDENT_NAME varchar2(20) not null
-, STUDENT_COMMENT varchar2(20)
-);
-
-alter table STUDENT add constraint sudent_pk PRIMARY KEY(STUDENT_EMAIL);
